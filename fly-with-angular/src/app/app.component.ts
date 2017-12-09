acd3b2c019e88cd52216f0ee6e245272729963d1
pranaysashank <pranaysashank@gmail.com>
2017-12-09 14:55:15 +0530

    save1

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Trip } from './model/trip';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'app';
+
+   //trip_name = 'xyz';
+
+//  trip_price = '1000/-';
+
+  //trip_duration = '50';
+
+
+
+  trip: Trip = {
+    name: 'Super  Flights to Mars',
+    price: 1300,
+    duration: '5 earth years',
+    description: 'Race through the Solar System to reach the Red planet.This flight will take 11 Earth Years for each side of the journey.',
+    url: 'https://download.pearltrees.com/s/pic/or/-115106467'
+  };
+
+
+
+
+
+
+
+
+
 }
